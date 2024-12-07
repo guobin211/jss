38c919fe67eb8cc5622ef00b2dfbcacc8e8b480d
michaelbguo <michaelbguo@tencent.com>
2024-12-07 16:49:12 +0800

    feat: init project

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub fn run_file() {}
+
+pub fn run_string() {}
+
+pub fn run_repl() {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("custom program language");
+    let mut args = std::env::args().collect::<Vec<String>>();
+    args.remove(0);
+    if args.len() == 1 {
+        rss::run_repl();
+    } else if args.len() == 2 {
+        rss::run_file();
+    } else if args.len() == 3 && args[1] == "eval" {
+        rss::run_string();
+    } else {
+        eprintln!("Usage:
+rss main.rss
+rss eval 1 + 2
+");
+    }
 }
